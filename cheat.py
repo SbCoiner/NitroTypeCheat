75fb22ca605511200f7dea4a85f4d68891997ec8
Noah <80302821+SbCoiner@users.noreply.github.com>
2021-09-15 16:51:17 -0700

    Allow cheat to make up for its mistakes
    Allows for more accurate WPM

--- cheat.py
+++ cheat.py
@@ -11,19 +11,30 @@ while True:
     text = input("Enter what u want to type: ")
     words = text.split(' ')
     used_nitro = False
+    made_up = 0.00
     print("Press shift to start")
     keyboard.wait('shift')
     time.sleep(0.2)
 
     for word in words:
-        if used_nitro == False and len(word) >= 10:
+        if (not used_nitro) and len(word) >= 10:
             keyboard.press_and_release('enter')
             continue
 
         rand = random.uniform(0.00, 100.00)
         if rand > accuracy:
             keyboard.write(random.choice(string.ascii_letters))
-            time.sleep(0.5)
+            made_up += 1
+            time.sleep(1)
 
         keyboard.write(word + ' ')
-        time.sleep(60/wpm)
+        
+        if made_up == 0.0:
+            time.sleep(60/wpm)
+        else:
+            if made_up >= 0.3:
+                time.sleep(60/wpm - 0.3)
+                made_up -= 0.3
+            else:
+                time.sleep(60/wpm - made_up)
+                made_up = 0.0
