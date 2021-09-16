f5a4653f0119117bbeb58754e6592ae1a606b8b7
Noah <80302821+SbCoiner@users.noreply.github.com>
2021-09-15 18:06:51 -0700

    Allow you to just press the bookmark

--- cheat.py
+++ cheat.py
@@ -2,20 +2,25 @@ import time
 import keyboard
 import random
 import string
+import pyperclip
 
 f = open("config.txt", 'r').readlines()
 wpm = float(f[0].replace(' ', '').replace('wpm=', ''))
 accuracy = float(f[1].replace(' ', '').replace('accuracy=', ''))
 
 while True:
-    text = input("Enter what u want to type: ")
-    words = text.split(' ')
     used_nitro = False
     made_up = 0.00
-    print("Press shift to start")
-    keyboard.wait('shift')
-    time.sleep(0.2)
+    prev_text = pyperclip.paste()
 
+    while True:
+        if pyperclip.paste() == prev_text:
+            continue
+        else:
+            words = pyperclip.paste().split('\xa0')
+            prev_text = pyperclip.paste()
+            break
+        
     for word in words:
         if (not used_nitro) and len(word) >= 10:
             keyboard.press_and_release('enter')
